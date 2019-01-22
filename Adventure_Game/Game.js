4aab29ba2d5cd55f5cd8cf24e78672cd1aec4d52
Unknown <aaron.campbell115@gmail.com>
2019-01-22 10:28:27 -0700

    more basics for running all the everything.

---
+++ Adventure_Game/Game.js
@@ -0,0 +1,17 @@
+//document.write("Hello World!");
+//document.write("<h1> hello bob.");
+// I was thinking maybe have my game be a player vs world game. start with the above bit?
+/*
+just practicing those comments
+multiline comments
+*/
+
+//alert("Warning bob... Warning!");
+//state the obvious
+//confirm("Do you like cheese?");
+// takes ok or cancel feedback
+//prompt("What kind of cheese?");
+// text feedback
+
+var PC = prompt("What is your name, miscreant?");
+document.write(PC);
