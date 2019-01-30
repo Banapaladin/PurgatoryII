bf9686c373a0f56424ce9f82e2e268dd924936a4
Unknown <aaron.campbell115@gmail.com>
2019-01-30 10:29:04 -0700

    inspect command

--- Adventure_Game/Game.js
+++ Adventure_Game/Game.js
@@ -30,7 +30,8 @@ function Game(){
         var temple = prompt("You wake up against a column in a building entirely made out of white marble. You are battered and bruised, and you cannot seem to remember why you are here. Where is this? Who are you? Questions that you may never remember the answer to. One thing is for sure, the Elf next to you probably shouldn’t have a sword in his face.").toLowerCase();
         
         if(temple == "look around" || temple == "look"){
-            var TempleLook = prompt("descriptive text, yaaaaay");
+            prompt("descriptive text, yaaaaay");
+            Temple();
         }
         
         else if(temple == "sleep"){
@@ -43,6 +44,45 @@ function Game(){
                 alert("game over!");
             }
                 }
+        
+        else if(temple == "inspect"){
+            var templeInspect = prompt("Inspect what?");
+            
+            if(templeInspect == "sword"){
+                alert("The slender white blade appears to be made entirely of mirror-like silver. It is the most incredible blade you have ever seen.");
+            }
+            else if(templeInspect == "elf"){
+                alert("this man appears to have been a fighter of some sort, with thick leather armor and two shortswords near his open hands. There is a longsword made entirely out of silver sticking out of his face, piercing through the marble floor beneath him.");
+            }
+            else if(templeInspect == "altar"){
+                alert("this once-massive marble altar has been smashed down the middle by some great force. There is a shredded book on the ground nearby.");
+            }
+            else if(templeInspect == "self"){
+                alert("You are wearing chainmail for some reason, and you have a long scabbard at your side. You still feel very sore for some unknown reason, perhaps you were in a fight with the elf?");
+                //I want to use this later so that, depending on WHEN they check on themself, they are more recovered.
+            }
+            else if(templeInspect == "room"){
+                alert("the building around you appears to be some sort of religious center, with high barreled ceilings, massive stained glass windows depicting beautiful scenes, and a shattered stone altar at the front. There is one exit, which appears to have been a large wooden door, and is now in smithereens around the gaping hole in the otherwise perfect southern wall.");
+            }
+            else if(templeInspect == "book"){
+                alert("the tome crumbles to dust in your hands. You couldn’t read the script on the front anyways.");
+            }
+            else("I don't know what "+templeInspect+" is!")
+            Temple();
+        }
+        
+        else if(temple == "take"){
+            var templeTake = prompt("What do you want to take?");
+        
+            if(templeTake == "sword"){
+                alert("The elegant blade pulls cleanly from the Elf’s skull. No blood is left on the blade. You relize that there is a scabbard at your side that fits this blade perfectly. What kind of person were you? You put the sword in the scabbard.");
+            }
+            else if(templeTake == ""){
+                
+            }
+        }
+                
+                
         else("I don't know what "+temple+" is!");
         Temple();
     }
