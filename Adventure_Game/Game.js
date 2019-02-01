6af5baa1660c7d1c507151ddfb9a9d6d2521e222
Unknown <aaron.campbell115@gmail.com>
2019-02-01 10:26:05 -0700

    bugfixes I guess?

--- Adventure_Game/Game.js
+++ Adventure_Game/Game.js
@@ -29,12 +29,7 @@ function Game(){
     function Temple(){
         var temple = prompt("You wake up against a column in a building entirely made out of white marble. You are battered and bruised, and you cannot seem to remember why you are here. Where is this? Who are you? Questions that you may never remember the answer to. One thing is for sure, the Elf next to you probably shouldn’t have a sword in his face.").toLowerCase();
         
-        if(temple == "look around" || temple == "look"){
-            prompt("descriptive text, yaaaaay");
-            Temple();
-        }
-        
-        else if(temple == "sleep"){
+        if(temple == "sleep"){
                 alert("you let unconciousness close back around you.");
             var resume = confirm("Do you wish to continue?");
             if(resume){
@@ -61,29 +56,44 @@ function Game(){
                 alert("You are wearing chainmail for some reason, and you have a long scabbard at your side. You still feel very sore for some unknown reason, perhaps you were in a fight with the elf?");
                 //I want to use this later so that, depending on WHEN they check on themself, they are more recovered.
             }
-            else if(templeInspect == "room"){
+            else if(templeInspect == "room" || templeInspect == "area"){
                 alert("the building around you appears to be some sort of religious center, with high barreled ceilings, massive stained glass windows depicting beautiful scenes, and a shattered stone altar at the front. There is one exit, which appears to have been a large wooden door, and is now in smithereens around the gaping hole in the otherwise perfect southern wall.");
             }
             else if(templeInspect == "book"){
-                alert("the tome crumbles to dust in your hands. You couldn’t read the script on the front anyways.");
-            }
-            else("I don't know what "+templeInspect+" is!")
+                alert("the tome crumbled to dust in your hands. You couldn’t read the script on the front anyways.");
+                }
+            
+            else(alert("I don't know what -"+templeInspect+"- is!"));
             Temple();
-        }
+            }
         
+        
+        //need to put in the inventory for this bit, otherwise it is useless.
         else if(temple == "take"){
             var templeTake = prompt("What do you want to take?");
         
             if(templeTake == "sword"){
                 alert("The elegant blade pulls cleanly from the Elf’s skull. No blood is left on the blade. You relize that there is a scabbard at your side that fits this blade perfectly. What kind of person were you? You put the sword in the scabbard.");
             }
-            else if(templeTake == ""){
-                
+            else if(templeTake == "shortsword" || templeTake == "shortswords"){
+                alert("You take both of the shorswords next to the Elf. They don't fit in your scabbard at all, so you strap them into your belt.");
             }
+            else if(templeTake == "armor" || templeTake == "leather armor"){
+                alert("Your chainmail is already better than this decimated armor, and it would take too long to get off him. you really need to figure out... something.");
+            }
+            else(alert("I don't think you can take that... nice try though."));
+            Temple();
         }
-                
-                
-        else("I don't know what "+temple+" is!");
+        
+        else if(temple == "cheat" || temple == "win"){
+            alert("You think you're smart, don't you?")
+        }
+        
+        else if(temple == ""){
+            
+        }
+    
+        else(alert("I don't know what -"+temple+"- is!"));
         Temple();
     }
 }
